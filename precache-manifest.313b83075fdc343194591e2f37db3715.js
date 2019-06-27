ccc8e6a6d46fddb85d66fd1da1ff2698941f71c5
Gerwin Jonathan <gerwinjh12345@gmail.com>
2019-06-27 20:41:28 +0700

    Updates

---
+++ precache-manifest.313b83075fdc343194591e2f37db3715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4f6542e0e287a7aef577aae0f70be9",
+    "url": "/my-login/index.html"
+  },
+  {
+    "revision": "ae6e54cfc7409212397c",
+    "url": "/my-login/static/css/main.29a03e72.chunk.css"
+  },
+  {
+    "revision": "41dca954624e8d221f9f",
+    "url": "/my-login/static/js/2.3d37b8f0.chunk.js"
+  },
+  {
+    "revision": "ae6e54cfc7409212397c",
+    "url": "/my-login/static/js/main.21268fc8.chunk.js"
+  },
+  {
+    "revision": "686e65e11d50a042a9b7",
+    "url": "/my-login/static/js/runtime~main.da3c06c3.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/my-login/static/media/React.47ce6e77.png"
+  },
+  {
+    "revision": "2ad58311f98382d9dec4ad0284bd4e17",
+    "url": "/my-login/static/media/lock.2ad58311.png"
+  }
+]);

40aeac2227c3df57da7237be373103029ae8180e
Gerwin Jo <43228839+gerwinjonathan@users.noreply.github.com>
2019-06-27 20:49:59 +0700

    Update LoginComponents.js

--- src/components/login/LoginComponents.js
+++ src/components/login/LoginComponents.js
@@ -37,8 +37,8 @@ export default class Login extends React.Component {
                     </nav>
             </div>
             <Switch>
-                <Route path="/my-login" component={LoginLogin} />
-                <Route path="/register" component={Register} />
+                <Route exact path="/my-login" component={LoginLogin} />
+                <Route path="/my-login/register" component={Register} />
             </Switch>
         </div>
         </Router>
